68195a448b2668357fa6f6ea09373ee93f6d8ed3
pashka555 <pprotasenya@gmail.com>
2019-05-13 15:11:33 +0300

    Added sounds, shadows, no rescaling, yet.

--- phaser-match3.js
+++ phaser-match3.js
@@ -110,13 +110,13 @@ function onCreate() {
 
     killSound = game.add.audio('kill');
 
-    var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-    alert(x + ' × ' + y);
+    //var w = window,
+    //d = document,
+    //e = d.documentElement,
+    //g = d.getElementsByTagName('body')[0],
+    //x = w.innerWidth || e.clientWidth || g.clientWidth,
+    //y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    //alert(x + ' × ' + y);
 
     //game.world.scale.x = x/1280;
     //game.world.scale.y = y/960;
